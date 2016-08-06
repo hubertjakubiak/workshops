c34a9407efdcb4e14eb7026fc122d424941ea774
Hubert <hubert.jakubiak@gmail.com>
2016-08-06 07:57:54 +0200

    Fix avg_notes decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -1,8 +1,16 @@
 class StudentDecorator < BaseDecorator
+  
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def avg_notes(subject_item)
+    if object.subject_item_notes.where(subject_item: subject_item).count == 0
+      return "0.00"
+    else
+      average = object.subject_item_notes.where(subject_item: subject_item).average(:value).to_s
+
+      return "#{format("%.2f", average)}"
+    end
   end
 end
