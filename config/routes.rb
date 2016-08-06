c206e834dedabd6be22a3877ad2de207c05e5ddf
Hubert <hubert.jakubiak@gmail.com>
2016-08-06 09:02:01 +0200

    Move subject_item partial to correct folder and setup route for report/subjects to pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   get '/reports/subjects', to: 'reports#subjects'
+  get '/report/subjects', to: 'reports#subjects', as: 'report_subjects'
   get '/visitors', to: 'visitors#index'
 
   root 'students#index'
